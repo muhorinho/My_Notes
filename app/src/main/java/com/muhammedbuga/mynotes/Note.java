4f8c8a638f959bb7a5a7ef5b577cde6308f2daa2
Muhammed <70836304+muhorinho@users.noreply.github.com>
2023-12-24 11:48:33 +0300

    Initial commit

---
+++ app/src/main/java/com/muhammedbuga/mynotes/Note.java
@@ -0,0 +1,35 @@
+package com.muhammedbuga.mynotes;
+
+import java.util.Date;
+
+public class Note {
+
+    private String header;
+    private Date date;
+    private String filePath;
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
+
